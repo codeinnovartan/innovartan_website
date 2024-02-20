690c22e08e46aada450049b6bf559b6ef2348b16
vanshvadhera <vansh.vadhera009@gmail.com>
2024-02-20 14:59:25 +0530

    Privacy Policy Page

--- src/Components/Pages/Privacy Policy/PrivacyPolicy.js
+++ src/Components/Pages/Privacy Policy/PrivacyPolicy.js
@@ -30,6 +30,60 @@ const PrivacyPolicy = () => {
           <PrivacyPolicyHeading heading="2. Information We Collect" />
           <PrivacyPolicyText text="For however long you’re on the Website/accessing the Website (“Information”), innovartan as per the voluntary registration process may collect the subsequent personal and non-personal information and such other information from the Users, any online survey or interaction on the Website or combination thereof, could also be required from time to time. The Information shall be collected to conduct operations on the Website. The Website collects or can request the below-mentioned Information from the Users." />
           <PrivacyPolicyText text="1.1 Personal Information:" />
+          <PrivacyPolicyText text="(i) User’s name;" />
+          <PrivacyPolicyText text="(ii) User’s phone number (mobile and/or alternative);" />
+          <PrivacyPolicyText text="(iii) User’s gender;" />
+          <PrivacyPolicyText text="(iv) DOB (Date of birth) of the User;" />
+          <PrivacyPolicyText text="(v) User’s address (office/residence/others);" />
+          <PrivacyPolicyText text="(vi) User’s email address (primary/alternative);" />
+          <PrivacyPolicyText text="(vii) Personal information collected from social media networking sites (name, profile picture, email address or friends list, and any information made public on that social media platform), should the User choose to register to the Website through it;" />
+          <PrivacyPolicyText text="(viii) User’s IP address;" />
+          <PrivacyPolicyText text="(ix) Sensitive personal data like password and payment details" />
+          <PrivacyPolicyText text="1.2 Non-Personal Information:" />
+          <PrivacyPolicyText text="(i) User’s internet or telecom service provider details;" />
+          <PrivacyPolicyText text="(ii) User’s location;" />
+          <PrivacyPolicyText text="(iii) User’s Internet browser that’s being used;" />
+          <PrivacyPolicyText text="(iv) Any such information that may be required to access and operate the Website" />
+          <PrivacyPolicyText text="1.3 Keep in mind that in addition to the above, the Website may also store and log the duration of your Website usage." />
+          <PrivacyPolicyText text="1.4 innovartan is hereby authorized by the User to collect/store Information in not only electronic form but physical form as well." />
+          <PrivacyPolicyText text="1.5 The User’s information is subjected to sharing with any third party entities by the Website, adopting reasonable safety standards concerning the use of such Information." />
+
+          <br />
+
+          <PrivacyPolicyHeading heading="3. Warranties By The User" />
+
+          <PrivacyPolicyText text="If the User wants to enjoy the Services provided on the Platform, registration is mandatory to complete the registration process, a legitimate mobile number would be required to register on the Platform, following which a one time password (OTP) would be sent to your mobile number to be shared by you at the time of registration for user validation." />
+
+          <PrivacyPolicyText text="At the time of downloading any content/material from the platform, you may also be asked to come up with an OTP.  In all cases, the OTP will appear on the mobile number you had provided at the time of registration. You take full ownership of any type of activity that takes place under your account. You specifically agree that the Company shall not be held accountable for unauthorized access to or alteration of your transmissions or data, any material or data sent or received or not sent or received through the Platform. By registering, you consent to share information including Personal Data." />
+          <PrivacyPolicyText text="Every User hereby warrants to innovartan that:" />
+          <PrivacyPolicyText text="(a) All information that you are providing us is true, correct, current and updated;" />
+          <PrivacyPolicyText text="(b) All Information and therefore the provision of such information you’re providing us doesn’t in any manner violate any third party agreement, law, decree, order or judgement;" />
+          <PrivacyPolicyText text="(c) All information you’re providing us doesn’t belong to any third party, and if it does belong to a third party, you’re duly authorized by such Third Party to use, access and disseminate such Information;" />
+          <PrivacyPolicyText text="(d)  innovartan’s officers, directors, contractors or agents shall not be responsible in any manner whatsoever regarding the authenticity or accuracy of the Information that you may provide to the Website; and" />
+          <PrivacyPolicyText text="(e) In the event of you being found in breach of this Policy, you shall compensate and hold innovartan and each of its officers, directors, contracts or agents and any third party counting on the Information provided by you, to be innocent." />
+          <PrivacyPolicyText text="innovartan warrants to each User that:" />
+          <PrivacyPolicyText text="(a) it shall not collect the User’s sensitive personal data unless it’s for a lawful purpose for which such collection of data is necessary;" />
+          <PrivacyPolicyText text="(b) it shall not hold any sensitive personal data for longer than it’s required or can be lawfully used;" />
+          <PrivacyPolicyText text="(c) If innovartan ever collects Information directly from the User, it shall make all efforts to brief the User of the purpose of such collection of Information, the intended recipient of the Information and also the details of the agencies collecting and retaining the Information; and" />
+          <PrivacyPolicyText text="(d) it has in place the safety practices and procedures prescribed under the Information Technology (Reasonable Security Practices and Procedures and Sensitive Personal Data or Information) Rules, 2011 (“IT Rules”)." />
+
+          <br />
+
+          <PrivacyPolicyHeading heading="4.  Managing Your Personal Information (Purpose and Use)" />
+          <PrivacyPolicyText text="All Information collected/stored by the Website shall be used for:" />
+          <PrivacyPolicyText text="(a) supplying Users information about new educational products and services;" />
+          <PrivacyPolicyText text="(b) consistently improve the current Website and its service offerings;" />
+          <PrivacyPolicyText text="(c) to perform research and surveys;" />
+          <PrivacyPolicyText text="(d) to carry out necessary security practices to make sure that all personal data are protected;" />
+          <PrivacyPolicyText text="(e) to administer the User accounts in the normal course of business;" />
+          <PrivacyPolicyText text="(f) to contact the Users in case of fraud, illegal activities or breach of privacy being observed or recorded;" />
+          <PrivacyPolicyText text="(g) to permit the staff of or persons acting on behalf of innovartan to speak with the User, as and when necessary to provide the services requested by such User;" />
+          <PrivacyPolicyText text="(h) such other purposes that innovartan, at its sole discretion, however subject to the principles contained in this Policy, may deem fit." />
+
+          <br />
+
+          <PrivacyPolicyHeading heading="5. Information We Share And Disclose" />
+          <PrivacyPolicyText text="You hereby agree that innovartan can share the information gathered from you with its employees, and such other individuals and institutions within or outside India from time to time to make sure efficient management of Website traffic, to search out and stop identity theft and other illegal acts, and to respond to legal, judicial, quasi-judicial law enforcement agencies or in reference to an investigation on matters associated with public safety, as required and permitted by law and for such other purposes that innovartan may deem fit from time to time. We don’t sell your personal information." />
         </div>
       </div>
     </div>
