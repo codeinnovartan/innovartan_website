46d720d6148f86e1fe9a264e2e8a82057e900463
vansh vadhera <vanshvadhera@vanshs-MacBook-Pro-2.local>
2024-08-27 13:07:58 +0530

    Changes in partner program

--- src/Components/Pages/Partner_Program/BecomeClassplus.js
+++ src/Components/Pages/Partner_Program/BecomeClassplus.js
@@ -6,8 +6,17 @@ const BecomeClassplus = () => {
     <div className={styles.container}>
       <h1 className={styles.BCheading}>
         Become an Innovartan <strong className={styles.Become}>Channel</strong>
-        <strong className={styles.A}>Partner</strong> Program Now
+        <strong className={styles.A}>Partner</strong>
       </h1>
+      <i
+        className="fa-regular fa-hand-point-down fa-2xl"
+        style={{
+          marginBottom: "30px",
+          marginTop: "30px",
+          fontSize: "50px",
+          color: "black",
+        }}
+      ></i>
       <p className={styles.BCdesc}>
         As an Innovartan Ambassador, I empower schools by educating educators on
         the advantages of hybrid learning, fostering a culture of innovation and
@@ -20,7 +29,7 @@ const BecomeClassplus = () => {
             "https://app.innovartan.com/affiliation/signup";
         }}
       >
-        Sign Up
+        Sign Up Now
       </button>
     </div>
   );
