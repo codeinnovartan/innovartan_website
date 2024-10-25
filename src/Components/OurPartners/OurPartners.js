606c1e3ab68c1ce7a1defa5e2c8e77790ab1d7bc
vivekinno <vivek.sharma@innovartan.com>
2024-10-25 17:21:34 +0530

    minor

--- src/Components/OurPartners/OurPartners.js
+++ src/Components/OurPartners/OurPartners.js
@@ -4,7 +4,7 @@ import styles from "./OurPartners.module.css";
 const OurPartners = () => {
   return (
     <div>
-      <p className={styles.partnerHeading}>Our Collaborators in Growth</p>
+      <p className={styles.partnerHeading}>Collaborators in Growth</p>
       <div className={styles.imagesContainer} >
         <img className={styles.partnerImage} src="Images/ourPartnerLogo1.png" alt="" />
         <img className={styles.partnerImage} src="Images/ourPartnerLogo2.svg" alt="" />
