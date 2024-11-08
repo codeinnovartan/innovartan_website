e27bea15553fb4ea0e092ca1ed636ed476fee3eb
vansh vadhera <vanshvadhera@vanshs-MacBook-Pro-2.local>
2024-11-08 14:02:09 +0530

    Email Sending Functionality

--- src/Components/Pages/Contact/ContactForm.js
+++ src/Components/Pages/Contact/ContactForm.js
@@ -1,111 +1,7 @@
-// import React, { useState } from "react";
-// import styles from "./ContactForm.module.css";
-// import { Button, Col, Form } from "react-bootstrap";
-// import { Row } from "react-bootstrap";
-
-// const ContactForm = () => {
-//   const [formData, setFormData] = useState({
-//     name: "",
-//     email: "",
-//     phone: "",
-//     subject: "",
-//     message: "",
-//   });
-
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormData({ ...formData, [name]: value });
-//   };
-
-//   const handleSubmite = () => {
-//     console.log(formData);
-//   };
-
-//   return (
-//     <>
-//       <div className={styles.contactForm}>
-//         <h2 className={styles.formSubtitle}>Get in touch with Contact us</h2>
-//         <h1 className={styles.formTitle}>
-//           Fill The Form Below So We Can Get To Know <br /> You And Your Needs
-//           Better.
-//         </h1>
-//         <Form>
-//           <Row>
-//             <Col md={6}>
-//               <Form.Group>
-//                 <Form.Control
-//                   type="text"
-//                   name="name"
-//                   className={styles.inputField}
-//                   placeholder="Your Name"
-//                   value={formData.name}
-//                   onChange={handleChange}
-//                 />
-//               </Form.Group>
-//             </Col>
-//             <Col md={6} className="mb-md-4">
-//               <Form.Group controlId="formBasicNumber">
-//                 <Form.Control
-//                   type="text"
-//                   name="email"
-//                   value={formData.email}
-//                   onChange={handleChange}
-//                   className={styles.inputField}
-//                   placeholder="Your Email"
-//                 />
-//               </Form.Group>
-//             </Col>
-//           </Row>
-//           <Row>
-//             <Col md={6} className="mb-md-4">
-//               <Form.Group controlId="formBasicName">
-//                 <Form.Control
-//                   type="text"
-//                   className={styles.inputField}
-//                   placeholder="Mobile Number"
-//                   name="phone"
-//                   value={formData.phone}
-//                   onChange={handleChange}
-//                 />
-//               </Form.Group>
-//             </Col>
-//             <Col md={6}>
-//               <Form.Group controlId="formBasicNumber">
-//                 <Form.Control
-//                   type="text"
-//                   className={styles.inputField}
-//                   placeholder="Your Subject"
-//                   name="subject"
-//                   value={formData.subject}
-//                   onChange={handleChange}
-//                 />
-//               </Form.Group>
-//             </Col>
-//           </Row>
-//           <Form.Group controlId="formBasicNumber">
-//             <Form.Control
-//               as="textarea"
-//               rows={10}
-//               name="message"
-//               value={formData.message}
-//               onChange={handleChange}
-//             />
-//           </Form.Group>
-//           <Button onClick={handleSubmite} variant='outline-warning' className={styles.submitButton}>
-//             Send Your Message
-//           </Button>
-//         </Form>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default ContactForm;
-
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styles from "./ContactForm.module.css";
 import { Button, Col, Form, Row } from "react-bootstrap";
-import { sendContactUs } from "../../Store/ApiCall";
+import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
@@ -115,20 +11,18 @@ const ContactForm = () => {
     subject: "",
     message: "",
   });
-
   const [errors, setErrors] = useState({});
   const [formSubmitted, setFormSubmitted] = useState(false);
+  const formRef = useRef(null); // Create a ref for the form element
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
-    // Clear the error message when the user starts typing again
     setErrors({ ...errors, [name]: "" });
   };
 
   const validateForm = () => {
     const newErrors = {};
-    // Validation rules
     if (!formData.name.trim()) {
       newErrors.name = "Name is required";
     }
@@ -147,21 +41,30 @@ const ContactForm = () => {
     }
 
     setErrors(newErrors);
-    // Return true if there are no errors
     return Object.keys(newErrors).length === 0;
   };
 
   const handleSubmite = async (e) => {
-    e.preventDefault(); // Prevent default form submission
+    e.preventDefault();
     const isValid = validateForm();
     if (isValid) {
-      console.log(formData);
-      const response = await sendContactUs(formData);
-      console.log(response);
-      if (response.statuscode === "200") {
-        setFormSubmitted(true);
-      }
-      // You can perform further actions like submitting the form data to a server here
+      setFormSubmitted(true);
+
+      emailjs
+        .sendForm(
+          "service_umf6b9d", // Your service ID
+          "template_97n4wig", // Your template ID
+          formRef.current, // Pass the form reference
+          "nAWs11i_i9eSe5baV" // Your public key
+        )
+        .then(
+          () => {
+            console.log("Email sent successfully!");
+          },
+          (error) => {
+            console.log("Email sending failed:", error);
+          }
+        );
     }
   };
 
@@ -172,7 +75,7 @@ const ContactForm = () => {
         Fill The Form Below So We Can Get To Know <br /> You And Your Needs
         Better.
       </h1>
-      <Form onSubmit={handleSubmite}>
+      <Form ref={formRef} onSubmit={handleSubmite}>
         <Row>
           <Col md={6}>
             <Form.Group>
