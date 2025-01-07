b495467d5fb165df24c1ce0da43eb639c7c9fa78
vansh vadhera <vanshvadhera@vanshs-MacBook-Pro-2.local>
2025-01-07 19:03:31 +0530

    link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,9 +16,9 @@ const Navbar = () => {
   return (
     <nav className="bg-white shadow-md sticky  top-0 z-50 font-metropolis">
       <div className="flex justify-between items-center mx-auto px-4 sm:px-6  max-w-7xl lg:px-8 py-6">
-       <Link to="/" className={getLinkClass("/")}>
-       <img src="/images/header-logo.svg" alt="logo" className="w-28 sm:w-auto" />
-          </Link>
+        <Link to="/" className={getLinkClass("/")}>
+          <img src="/images/header-logo.svg" alt="logo" className="w-28 sm:w-auto" />
+        </Link>
 
         <div className="flex items-center space-x-4">
           <button className="bg-[#F36421] text-white px-4 py-2 rounded-xl lg:hidden font-metropolis hover:text-[#F36421] hover:border hover:border-[#F36421] hover:bg-transparent transition ease-in duration-300">
@@ -29,12 +29,12 @@ const Navbar = () => {
             onClick={toggleMenu}
             aria-label="Toggle Menu"
           >
-            {isOpen ? <FiX className="text-[#0743A3]"/> : <FiMenu className="text-[#0743A3]"/>}
+            {isOpen ? <FiX className="text-[#0743A3]" /> : <FiMenu className="text-[#0743A3]" />}
           </button>
         </div>
 
         <div className="hidden lg:flex space-x-6 font-medium font-metropolis">
-        <Link to="/" className={getLinkClass("/")}>
+          <Link to="/" className={getLinkClass("/")}>
             Home
           </Link>
           <Link to="/schools" className={getLinkClass("/schools")}>
@@ -54,10 +54,12 @@ const Navbar = () => {
           </Link>
         </div>
         <div className="hidden lg:flex space-x-4 text-base font-medium font-metropolis">
-          <button  id="#talk-to-us"className="bg-[#F36421] text-white px-4 py-2 rounded-xl hover:text-[#F36421] hover:border hover:border-[#F36421] hover:bg-transparent transition ease-in duration-300">
+          <button id="#talk-to-us" className="bg-[#F36421] text-white px-4 py-2 rounded-xl hover:text-[#F36421] hover:border hover:border-[#F36421] hover:bg-transparent transition ease-in duration-300">
             Talk to Us
           </button>
-          <button className="bg-[#0743A3] text-white px-4 py-2 rounded-xl hover:text-[#0743A3] hover:border hover:border-[#0743A3] hover:bg-transparent transition ease-in duration-300">
+          <button className="bg-[#0743A3] text-white px-4 py-2 rounded-xl hover:text-[#0743A3] hover:border hover:border-[#0743A3] hover:bg-transparent transition ease-in duration-300" onClick={() => {
+            window.location.href = "https://tests.innovartan.com"
+          }} >
             Login/Signup
           </button>
         </div>
