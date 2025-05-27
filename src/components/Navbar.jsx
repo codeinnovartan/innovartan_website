8716f18efc986d4d659b1bfbc997e0fb48933cbe
laxmisingh02innovartan <laxmi.singh@innovartan.com>
2025-05-27 13:31:52 +0530

    web

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -87,7 +87,7 @@ const Navbar = () => {
             Talk to Us
           </button>
           <button className="bg-[#0743A3] text-white px-4 py-2 rounded-xl border border-transparent hover:text-[#0743A3] hover:border hover:border-[#0743A3] cursor-pointer hover:bg-transparent transition ease-in duration-300"  onClick={() => {
-                window.open("https://tests.innovartan.com")
+                window.open("https://web.innovartan.com")
             }
             }>
             Login
@@ -138,7 +138,7 @@ const Navbar = () => {
           <button
             className="bg-[#0743A3] text-white px-4 py-2 rounded-xl border border-transparent hover:text-[#0743A3] hover:border hover:border-[#0743A3] hover:bg-transparent transition ease-in duration-300 cursor-pointer"
             onClick={() => {
-                window.open("https://tests.innovartan.com")
+                window.open("https://web.innovartan.com")
             }
             }
           >
